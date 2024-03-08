950fe967b3c409460c72efb46d7896389ab849c0
sriwahyunilkhotimah <114728068+sriwahyunilkhotimah@users.noreply.github.com>
2024-03-08 22:33:06 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,15 @@
+public class Shape {
+    protected Double area;
+    protected Double perimeter;
+    protected String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
